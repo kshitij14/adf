de6360d2e83b7f4e38feb34cf0e0abf479aa5899
kshitij14 <kshitij14395@yahoo.co.in>
2014-06-14 12:20:22 +0530

    Update bb.h

--- bb.h
+++ bb.h
@@ -7,27 +7,27 @@
 //pin definations
 
 #define BITBANG_CE_PxDIR       P1DIR  //P4DIR                     
-#define BITBANG_CE_PxOUT   P1OUT//P4OUT                     //
-#define BITBANG_CE_BIT     BIT1//BIT5                   //  
+#define BITBANG_CE_PxOUT   P1OUT//P4OUT                     
+#define BITBANG_CE_BIT     BIT1//BIT5                     
 
-#define BITBANG_SREAD_PxDIR  P1DIR  //P1DIR               //Port10. pin 2
+#define BITBANG_SREAD_PxDIR  P1DIR  //P1DIR             
 #define BITBANG_SREAD_PxOUT  P1OUT//P1OUT
 #define BITBANG_SREAD_BIT    BIT4//BIT7                       
 #define BITBANG_SREAD_PxIN  P1IN
 #define BITBANG_SREAD_PxREN P1REN
 
-#define BITBANG_SLE_PxDIR   P1DIR//P5DIR                    //
-#define BITBANG_SLE_PxOUT   P1OUT//P5OUT                    //
-#define BITBANG_SLE_BIT     BIT2//BIT5                    //
+#define BITBANG_SLE_PxDIR   P1DIR//P5DIR                
+#define BITBANG_SLE_PxOUT   P1OUT//P5OUT                
+#define BITBANG_SLE_BIT     BIT2//BIT5                  
 
-#define BITBANG_SDATA_PxDIR  P1DIR//P4DIR                   //
-#define BITBANG_SDATA_PxOUT  P1OUT//P4OUT                   //
-#define BITBANG_SDATA_BIT    BIT0//BIT0                   //
-#define BITBANG_SDATA_PxREN  P1REN                              //NEWSTUFF
+#define BITBANG_SDATA_PxDIR  P1DIR//P4DIR               
+#define BITBANG_SDATA_PxOUT  P1OUT//P4OUT               
+#define BITBANG_SDATA_BIT    BIT0//BIT0                 
+#define BITBANG_SDATA_PxREN  P1REN                      
 
-#define BITBANG_SCLK_PxDIR   P1DIR//P4DIR                   //
-#define BITBANG_SCLK_PxOUT   P1OUT//P4OUT                   //
-#define BITBANG_SCLK_BIT     BIT3//BIT3                   //
+#define BITBANG_SCLK_PxDIR   P1DIR//P4DIR               
+#define BITBANG_SCLK_PxOUT   P1OUT//P4OUT               
+#define BITBANG_SCLK_BIT     BIT3//BIT3                   
 
 
 //prototyping functions:
